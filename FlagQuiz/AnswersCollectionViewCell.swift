e4ca5eeff1c382b4b4ab4615d38b28d4ce0ce067
Ivan Jovanovik <Ivan.jovanovik@vpar.com>
2023-10-12 08:41:30 +0200

    Fix crashres and update UI for Vojdan's iPhone 7

---
+++ FlagQuiz/AnswersCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  AnswersCollectionViewCell.swift
+//  FlagQuiz
+//
+//  Created by Ivan Jovanovik on 30/11/2022.
+//
+
+import UIKit
+
+class AnswersCollectionViewCell: UICollectionViewCell {
+
+  
+    @IBOutlet weak var backgroundCellView: UIView!
+    @IBOutlet weak var answerLbl: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
--- FlagQuiz/ViewController.swift
+++ FlagQuiz/ViewController.swift
@@ -10,73 +10,110 @@ import UIKit
 class ViewController: UIViewController {
     
     
+    @IBOutlet weak var nextCountryBtn: UIButton!
     @IBOutlet weak var countryName: UILabel!
     @IBOutlet weak var countryFlag: UIImageView!
-    var countryArray: [String] = []
-    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var countryObject: [String] = []
+    var rightAnswer: String = ""
+    var csvRows: [[String]] = []
+    var answersArray: [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        nextCountryBtn.layer.cornerRadius = 10
         foundCountry()
-        print(countryArray) //UXM n. 166/167.
+        setupCollectionView()
+        print(countryObject) //UXM n. 166/167.
     }
     
     @IBAction func chooseNextCountry(_ sender: Any) {
         foundCountry()
     }
     @objc func foundCountry(){
-        var data = readDataFromCSV(fileName: "countryNamesWithTranslate", fileType: "csv")
-        data = cleanRows(file: data!)
-        let csvRows = csv(data: data!)
+        rightAnswer = ""
+        var data = Utils.shared.readDataFromCSV(fileName: "countryNamesWithTranslate", fileType: "csv")
+        data = Utils.shared.cleanRows(file: data!)
+        csvRows = Utils.shared.csv(data: data!)
         
         let randomCountry = Int(arc4random_uniform(245) + 1)
         
         let oneCountry = csvRows[randomCountry].first
         if let oneCountry = oneCountry {
-            countryArray = oneCountry.components(separatedBy: ",")
+            countryObject = oneCountry.components(separatedBy: ",")
         }
-        countryFlag.image = UIImage(named: countryArray[1])
-        countryName.text = countryArray[2]
-        debugPrint("CountryFlagName: \(countryArray[1])")
+        countryFlag.image = UIImage(named: countryObject[1])
+//        countryName.text = countryObject[2]
+        rightAnswer = countryObject[2]
+        debugPrint("CountryFlagName: \(countryObject[1])")
+        getAnswersArray()
+        collectionView.reloadData()
+    }
+    
+    func getAnswersArray(){
+        var answersArray: [String] = []
+        answersArray.append(countryObject[2])
+        
+        for _ in 1...3 {
+            let randomCountry = Int(arc4random_uniform(245) + 1)
+            let oneCountry = csvRows[randomCountry].first
+            var helperCountryObject : [String] = []
+            if let oneCountry = oneCountry {
+                helperCountryObject = oneCountry.components(separatedBy: ",")
+            }
+            if rightAnswer != helperCountryObject[2] {
+                answersArray.append(helperCountryObject[2])
+            }
+        }
+        answersArray.shuffle()
+        self.answersArray = answersArray
+    }
+    
+    func setupCollectionView(){
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .vertical
+        collectionLayout.itemSize = CGSize(width: (view.frame.size.width / 2.5), height: 40)
+        collectionLayout.minimumInteritemSpacing = 2
+        collectionLayout.minimumLineSpacing = 2
+        
+        collectionView.collectionViewLayout = collectionLayout
+        
+        collectionView.register(UINib(nibName: "AnswersCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: "AnswersCollectionViewCell")
+    }
+}
+
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "AnswersCollectionViewCell", for: indexPath) as? AnswersCollectionViewCell {
+            
+            cell.answerLbl.text = self.answersArray[indexPath.row]
+            cell.answerLbl.textColor = .black
+            cell.backgroundCellView?.backgroundColor = .white
+            return cell
+        }
+        return UICollectionViewCell()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
+        if let cell = collectionView.cellForItem(at: indexPath) as? AnswersCollectionViewCell {
+            
+            if self.answersArray[indexPath.row] == rightAnswer {
+                cell.backgroundCellView?.backgroundColor = .green
+            } else {
+                cell.backgroundCellView?.backgroundColor = .red
+            }
+        }
+        
         
     }
-
-    
-    func csv(data: String) -> [[String]] {
-        var result: [[String]] = []
-        let rows = data.components(separatedBy: "\n")
-        for row in rows {
-            let columns = row.components(separatedBy: ";")
-            result.append(columns)
-        }
-        return result
-    }
-    
-    func readDataFromCSV(fileName:String, fileType: String)-> String!{
-            guard let filepath = Bundle.main.path(forResource: fileName, ofType: fileType)
-                else {
-                    return nil
-            }
-            do {
-                var contents = try String(contentsOfFile: filepath, encoding: .utf8)
-                contents = cleanRows(file: contents)
-                return contents
-            } catch {
-                print("File Read Error for file \(filepath)")
-                return nil
-            }
-        }
-
-
-    func cleanRows(file:String)->String{
-        var cleanFile = file
-        cleanFile = cleanFile.replacingOccurrences(of: "\r", with: "\n")
-        cleanFile = cleanFile.replacingOccurrences(of: "\n\n", with: "\n")
-        //        cleanFile = cleanFile.replacingOccurrences(of: ";;", with: "")
-        //        cleanFile = cleanFile.replacingOccurrences(of: ";\n", with: "")
-        return cleanFile
-    }
-
 }
 
